edecf81929d0c8099974af9b88352b7d6d482c8c
Bernhard Reitinger <br@rexos.org>
2020-03-17 11:57:24 +0100

    Move query constants to a simple query function

--- rexos/constants.go
+++ rexos/constants.go
@@ -1,15 +1,5 @@
 package rexos
 
-var (
-	apiFindByKey          = "/search/findByKey?key="
-	apiFindByNameAndOwner = "/search/findByNameAndOwner?"
-	apiFindByOwner        = "/search/findAllByOwner?owner="
-	apiFindByParent       = "/search/findAllByParentReferenceAndCategory?parentReference="
-	apiFindByUrn          = "/search/findByUrn?urn="
-	apiAndCategory        = "&category="
-	apiAndPage            = "&page="
-)
-
 const (
 	RexSchemeV1 = "rexos.scheme.v1"
 	RexFileType = "rex"
---
+++ rexos/query.go
@@ -0,0 +1,16 @@
+package rexos
+
+// QueryFindByKey generates a FindByKey query
+func QueryFindByKey(base, key string) string {
+	return base + "/search/findByKey?key=" + key
+}
+
+// QueryFindByUrn generates a FindByUrn query
+func QueryFindByUrn(base, urn string) string {
+	return base + "/search/findByUrn?urn=" + urn
+}
+
+// QueryFindByParentReferenceAndCategory generates a query for getting a parent ref with category
+func QueryFindByParentReferenceAndCategory(base, parent, category string) string {
+	return base + "/search/findAllByParentReferenceAndCategory?parentReference=" + parent + "&category=" + category
+}
