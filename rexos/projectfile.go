ba3046a8591daa9f76bff7fb00b2376f34156c21
Bernhard Reitinger <br@rexos.org>
2020-05-20 19:57:35 +0200

    Add datatransformation for projectfile (#8)

--- rexos/projectfile.go
+++ rexos/projectfile.go
@@ -1,8 +1,11 @@
 package rexos
 
+import "github.com/roboticeyes/gococo/math"
+
 // ProjectFile is a simple structure for carry binary meta-data
 type ProjectFile struct {
-	Name    string `json:"name"`
-	Project string `json:"project"`
-	Type    string `json:"type"`
+	Name               string              `json:"name"`
+	Project            string              `json:"project"`
+	DataTransformation math.Transformation `json:"dataTransformation"`
+	Type               string              `json:"type"`
 }
